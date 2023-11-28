1611b27501ce86159c527a22fbcdbd3a5b56513b
FFFiend <owais.zahid@mail.utoronto.ca>
2023-11-27 22:48:45 -0500

    fixed forward method, added gates

--- rnn.py
+++ rnn.py
@@ -3,6 +3,8 @@ File for the base Recurrent-Neural-Net model. NO IMPORTS ALLOWED!
 """
 
 import numpy as np
+from gates import MatrixMultGate, AddGate
+from nonlinearity import Softmax
 
 HIDDEN_LAYER_SIZE = 100
 RANDOMIZER_CONSTANT = 1250
@@ -10,8 +12,15 @@ LEARNING_RATE = 0.001
 
 class Base_RNN:
     def __init__(self, input_dims, output_dims, hidden_layer_size=HIDDEN_LAYER_SIZE):
+        """
+        Initializes the RNN, given input, output dims and hidden layer size.
+
+        Weights are initialized to random values, and bias vectors are all just 1s.
+        """
         # first mult, Ux
-        
+        # RANDOMIZER_CONSTANT to make sure initial weights aren't too large
+
+
         self.U = np.randn(hidden_layer_size, input_dims) / RANDOMIZER_CONSTANT
 
         # intermediary 
@@ -22,27 +31,22 @@ class Base_RNN:
 
         #biases
         self.b1 = np.zeros((hidden_layer_size, 1))
-        self.b2 = np.zeroes((output_dims, 1))
+        self.b2 = np.zeros((output_dims, 1))
 
         self._hidden_layer_size = hidden_layer_size
+        self._input_dims = input_dims
 
     
-    def forward_pass(self, input):
+    def forward_pass(self, prev_state, input):
         """
         Forward pass for the RNN
         """
-        h = np.zeros((self._hidden_layer_size, 1))
-        prev_timestep_h = {0:h}
+        self.u_ = MatrixMultGate.forward(self.U, input)
+        self.w_ = MatrixMultGate.forward(self.W, prev_state)
 
-        for i, x in enumerate(input):
-            # tanh range is [-1,1] so im not gonna use sigmoid unless im doing
-            # binary classification.
-            h = np.tanh(self.W @ x + self.U @ h + self.b1)
-            prev_timestep_h[i+1] = h
-        
-        z = self.V @ h + self.b2
+        self.add = np.tanh(AddGate.forward(AddGate.forward(self.u_, self.w_),self.b1))
 
-        return z, h
+        self.z = AddGate.forward(MatrixMultGate.forward(self.V, self.add), self.b2)
 
     def backprop(self):
         
